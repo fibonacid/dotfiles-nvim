2a7571a539a9e4fb75e9fca47f3440ffa42b144d
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2025-01-05 18:04:23 +0100

    keep buffer centered when navigating with {}

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,5 +19,9 @@ keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tav 
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tav
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- center buffer when moving up or down
+keymap.set("n", "{", "{zz", { noremap = true, silent = true })
+keymap.set("n", "}", "}zz", { noremap = true, silent = true })
