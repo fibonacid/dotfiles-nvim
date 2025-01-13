947cfb99bbd2b482b1cd66063e9dc48c5955c7f8
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2025-01-13 19:58:10 +0100

    add keymap to facilitate gnc based substitutions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,8 +20,16 @@ keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- o
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tav
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open urrent buffer in new tab" }) --  move current buffer to new tab
 
 -- center buffer when moving up or down
 keymap.set("n", "{", "{zz", { noremap = true, silent = true })
 keymap.set("n", "}", "}zz", { noremap = true, silent = true })
+
+-- replace all occurrences of word under cursor
+keymap.set(
+	"n",
+	"ra",
+	"g*:%s/<C-r>//<C-r>//gI<Left><Left><Left>",
+	{ desc = "Replace all occurrences of word under cursor" }
+)
