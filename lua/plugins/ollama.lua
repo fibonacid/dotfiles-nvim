75f6d142f2c7b5b0c71e716ea0f66792dafc3d23
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2025-07-10 17:11:59 +0200

    change ollama config

--- lua/plugins/ollama.lua
+++ lua/plugins/ollama.lua
@@ -27,6 +27,23 @@ return {
 
 	---@type Ollama.Config
 	opts = {
-		-- your configuration overrides
+		model = "codellama:7b",
+		url = "http://127.0.0.1:11434",
+		serve = {
+			on_start = false,
+			command = "ollama",
+			args = { "serve" },
+			stop_command = "pkill",
+			stop_args = { "-SIGTERM", "ollama" },
+		},
+		-- View the actual default prompts in ./lua/ollama/prompts.lua
+		prompts = {
+			Sample_Prompt = {
+				prompt = "This is a sample prompt that receives $input and $sel(ection), among others.",
+				input_label = "> ",
+				model = "mistral",
+				action = "display",
+			},
+		},
 	},
 }
