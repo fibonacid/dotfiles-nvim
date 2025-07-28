d573c54f3d1107447314251744fe869ade1c0b18
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2025-07-28 18:37:27 +0200

    add more tab keymaps

--- init.lua
+++ init.lua
@@ -12,7 +12,12 @@ vim.keymap.set("n", "<leader>o", ":update<CR> :source<CR>", { desc = "Save and r
 vim.keymap.set("n", "<leader>w", ":write<CR>", { desc = "Save" })
 vim.keymap.set("n", "<leader>q", ":quit<CR>", { desc = "Quit" })
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear search highlights" })
-vim.keymap.set("n", "<leader>t", ":tabnew<CR>", { desc = "Open new tab" })
+
+-- Tabs and Splits
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>", { desc = "Open new tab" })
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>tn", ":tabnext<CR>", { desc = "Next tab" })
+vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>", { desc = "Previous tab" })
 vim.keymap.set("n", "<leader>v", ":vsplit<CR>", { desc = "Split vertically" })
 vim.keymap.set("n", "<leader>c", ":hsplit<CR>", { desc = "Split horizontally" })
 
