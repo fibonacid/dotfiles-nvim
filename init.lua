4c614ba1834099798b62852fb1d45618114d5759
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2025-07-28 18:23:34 +0200

    add copilot

--- init.lua
+++ init.lua
@@ -16,6 +16,7 @@ vim.keymap.set("n", "<leader>q", ":quit<CR>", { desc = "Quit" })
 vim.pack.add({
 	{ src = "https://github.com/rose-pine/neovim" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/github/copilot.vim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
