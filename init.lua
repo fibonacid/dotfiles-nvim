423a4c42bfb0f5a62ad38ef22dee236fa47bcf7e
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2025-07-28 18:04:33 +0200

    add lsp and other stuff

--- init.lua
+++ init.lua
@@ -4,14 +4,32 @@ vim.o.swapfile = false
 vim.o.wrap = false
 vim.o.tabstop = 2
 vim.g.mapleader = " "
+vim.o.signcolumn = "yes"
 
 
 vim.keymap.set("n", "<leader>o", ":update<CR> :source<CR>")
+vim.keymap.set("n", "<leader>w", ":write<CR>")
+vim.keymap.set("n", "<leader>q", ":quit<CR>")
 
 vim.pack.add({
-				{ src = "https://github.com/rose-pine/neovim" }
+	{ src = "https://github.com/rose-pine/neovim" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/stevearc/oil.nvim" }
 })
+
 
 vim.cmd("colorscheme rose-pine-moon")
 vim.cmd(":hi statusline guibg=NONE")
+
+vim.lsp.enable({
+	"lua_ls",
+	"ts_ls",
+	"basedpyright"
+})
+
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+
+require("oil").setup()
+vim.keymap.set("n", "<leader>e", ":Oil<CR>")
+
 
