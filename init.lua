e1181e9e88555438a44864fb8775464aca838ed1
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2025-08-01 01:37:27 +0200

    add autocomplete

--- init.lua
+++ init.lua
@@ -48,6 +48,46 @@ vim.lsp.enable({
 	"yamlls",
 })
 
+--
+-- Auto Complete
+-- Works best with completeopt=noselect.
+-- Use CTRL-Y to select an item. |complete_CTRL-Y|
+vim.cmd [[set completeopt+=menuone,noselect,popup]]
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('my.lsp', {}),
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client:supports_method('textDocument/implementation') then
+			-- Create a keymap for vim.lsp.buf.implementation ...
+		end
+
+		-- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+		if client:supports_method('textDocument/completion') then
+			print('Enabling autocompletion for ' .. client.name)
+			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
+			-- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+			-- client.server_capabilities.completionProvider.triggerCharacters = chars
+
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
+		print('LSP client attached: ' .. client.name)
+
+		-- Auto-format ("lint") on save.
+		-- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+		if not client:supports_method('textDocument/willSaveWaitUntil')
+		    and client:supports_method('textDocument/formatting') then
+			vim.api.nvim_create_autocmd('BufWritePre', {
+				group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+				buffer = args.buf,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+				end,
+			})
+		end
+	end,
+})
+
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
@@ -112,7 +152,8 @@ vim.lsp.config('ts_ls', {
 	on_attach = function(client, bufnr)
 		if client.name == "ts_ls" then
 			vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { buffer = bufnr, desc = "Format file" })
-			vim.keymap.set("n", "<leader>li", ":LspTypescriptSourceAction<CR>", { buffer = bufnr, desc = "Source action" })
+			vim.keymap.set("n", "<leader>li", ":LspTypescriptSourceAction<CR>",
+				{ buffer = bufnr, desc = "Source action" })
 		end
 	end,
 })
