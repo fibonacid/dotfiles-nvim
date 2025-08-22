02ea22681dead05774ae18ccc61fae3c3d71a880
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2025-08-22 21:06:04 +0200

    set cli config flag

--- lsp/arduino_language_server.lua
+++ lsp/arduino_language_server.lua
@@ -71,20 +71,20 @@
 local util = require 'lspconfig.util'
 
 return {
-  filetypes = { 'arduino' },
-  root_dir = function(bufnr, on_dir)
-    local fname = vim.api.nvim_buf_get_name(bufnr)
-    on_dir(util.root_pattern('*.ino')(fname))
-  end,
-  cmd = {
-    'arduino-language-server',
-  },
-  capabilities = {
-    textDocument = {
-      semanticTokens = vim.NIL,
-    },
-    workspace = {
-      semanticTokens = vim.NIL,
-    },
-  },
+	filetypes = { 'arduino' },
+	root_dir = function(bufnr, on_dir)
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+		on_dir(util.root_pattern('*.ino')(fname))
+	end,
+	cmd = {
+		'arduino-language-server --cli-config /Users/lorenzo/Library/Arduino15/arduino-cli.yaml',
+	},
+	capabilities = {
+		textDocument = {
+			semanticTokens = vim.NIL,
+		},
+		workspace = {
+			semanticTokens = vim.NIL,
+		},
+	},
 }
